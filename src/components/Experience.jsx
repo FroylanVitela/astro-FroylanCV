722b221dc2e9ef9d55ac59d0482bd05da0c7394b
Froylán Vitela <vvitelacuevas@gmail.com>
2025-11-13 23:30:02 -0600

    XP Update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,18 +15,25 @@ export default function Experience() {
       logo: "https://electroniccats.com/openhardwaremonth/static/logo_electroniccats-c14f4b90646a1705f118e69f56070c40.png",
       descripcion:
         "Automatización de procesos internos con Make, Airtable y Discord Webhooks. Implementación de flujos de ventas, notificaciones y CRM.",
-      fecha: "ENE 2024 - ABR 2025"
+      fecha: "ENE 2025 - ABR 2025"
     },
     {
       empresa: "QMC BEYOND & QUALITY - Soporte Técnico",
       logo: "https://qmcmex.com/public/imgs/logos/QMCB.png",
       descripcion:
       "Atención directa a incidentes técnicos de los empleados. Diagnóstico y resolución de fallas de hardware y software. Canalización de incidencias complejas al personal especializado para su resolución. Experiencia en mantenimiento de equipos y trato con usuarios. Experiencia en la implementación de soluciones tecnológicas para optimizar el rendimiento del equipo. Apoyo en la gestión de proyectos tecnológicos y la capacitación del personal en el uso de nuevas herramientas.",
-      fecha: "JUL 2024 - Actualidad"
+      fecha: "JUL 2025 - SEP 2025"
+    },
+    {
+      empresa: "Jatco México - Mejora de procesos",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Jatco_logo.svg/250px-Jatco_logo.svg.png",
+      descripcion:
+      "Diseñé e implementé soluciones operativas integradas, incluyendo un sistema modular en Excel/VBA para gestión de inventarios y un modelo de base de datos unificado. Coordiné equipos multidisciplinarios para alinear requerimientos funcionales, optimizar flujos de trabajo y presentar avances estratégicos a nivel ejecutivo.",
+      fecha: "SEP 2025 - Actualidad"
     },
     {
       empresa: "Dev Freelance - Desarrollo Web",
-      logo: "https://img.icons8.com/?size=100&id=CjJ8TXg2lg1j&format=png&color=000000",
+      logo: "https://cdn-icons-png.flaticon.com/512/1995/1995437.png",
       descripcion:
         "Diseño y desarrollo de sitios web responsivos y totalmente funcionales, principalmente para proyectos escolares pero con la posibilidad de aplicarlo al mundo real. Implementación de tecnologías modernas como React, Next.js y Tailwind CSS. Creación de aplicaciones web interactivas y optimizadas. Documentación y mantenimiento de proyectos existentes. Dockerización de aplicaciones para facilitar el despliegue y la escalabilidad.",
       fecha: "2023 - Actualidad"
